f18df627c8fe2131272c254782eea797f9413204
AakashThakur23102000 <85384846+AakashThakur23102000@users.noreply.github.com>
2021-06-05 12:22:23 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        JSX allows us to write HTML elements in JavaScript and place them in the
+        DOM without any createElement() and/or appendChild() methods.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
